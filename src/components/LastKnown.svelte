f918eda3d291cc567c95f78d88c9dc06c00f275f
Khang Nguyen <khangnguyen@Khangs-MacBook-Air.local>
2022-02-22 11:10:06 -0500

    Working date

--- src/components/LastKnown.svelte
+++ src/components/LastKnown.svelte
@@ -1,1 +1,33 @@
-<div>Last Known</div>
+<script>
+  let list = [];
+  let newItem;
+
+  const dateOptions = { dateStyle: 'long' ,timeStyle: 'medium' };
+
+  const handleOnclick = () => {
+    list = [...list, {
+      name: newItem,
+      lastKnown: Intl.DateTimeFormat('en-US', dateOptions).format(new Date()),
+    }];
+  }
+
+  const logLastKnown = (idx) => {
+    list[idx].lastKnown = Intl.DateTimeFormat('en-US', dateOptions).format(new Date());
+  }
+
+</script>
+
+<div>
+  <input type="text" placeholder="New item name" bind:value={newItem}/>
+  <button on:click={handleOnclick}>Create</button>
+</div>
+
+{#if list.length > 0}
+  {#each list as item, i}
+    <li>
+      <span>{item.name}</span>
+      <span>{item.lastKnown}</span>
+      <button on:click={() => logLastKnown(i)}>Log last known</button>
+    </li>
+  {/each}
+{/if}
