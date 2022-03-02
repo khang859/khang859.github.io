f040e7aac80a488e4943c3543bc991340f44ae40
Khang Nguyen <khangnguyen@Khangs-MacBook-Air.local>
2022-03-02 13:58:48 -0500

    About page

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,1 +1,29 @@
-<div>About me</div>
+<script>
+  import InstantAnalysisIcon from './InstantAnalysisIcon.svelte';
+</script>
+<section>
+  <h1 class="text-2xl font-bold my-4">So you want to learn more about me, I am flattered</h1>
+  <p>My name is Khang Nguyen incase you haven't seen it all over the site already.</p>
+</section>
+
+<section class="w-full text-lg mt-20">
+  <div>
+    <h2 class="text-xl font-semibold">Professionally: </h2>
+    <p class="mt-2">I am a Software Engineer by trade. I've been developing software since my college days, talk about lucky, picking the right major at the right time.</p>
+    <p class="mt-2">I enjoy physically typing code, but architecting new solutions is much more fun in my opinion.</p>
+    <p class="mt-2">I mainly use SvelteJS, ReactJS, NodeJS all kinds of JSes (new ones everyday)</p>
+  </div>
+
+  <div class="mt-8">
+    <h2 class="text-xl font-semibold">Personally: </h2>
+    <p class="mt-2">I watch way too much anime and play too many video games, luckily my wife also play videos and watch anime, win win.</p>
+    
+  </div>
+</section>
+
+<section class="w-full mt-20 flex flex-col justify-center">
+  <h2 class="flex justify-center">Heres an image of me doing something incredibly cool to fill up the space of this page.</h2>
+  <div class="max-w-lg w-full flex mx-auto mt-4 justify-center">
+    <InstantAnalysisIcon />
+  </div>
+</section>
